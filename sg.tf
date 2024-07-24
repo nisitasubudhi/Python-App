c930746e5a219d84c0354b7fd63203c15bf51a04
nisitasubudhi <162950721+nisitasubudhi@users.noreply.github.com>
2024-07-24 17:44:40 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -14,6 +14,12 @@ resource "aws_security_group" "demosg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 egress {
     from_port       = 0
     to_port         = 0
