cbf22b0461552a386f3848b4d6e10810128b407f
nisitasubudhi <162950721+nisitasubudhi@users.noreply.github.com>
2024-07-24 17:32:20 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/nisitasubudhi/fish.git
+git clone https://github.com/nisitasubudhi/Penguin-.git
 sudo yum -y install python3-pip
-cd /fish
+cd /Penguin-
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
