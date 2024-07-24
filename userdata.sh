dc3111eb55fd4f2e41421caafee939c9d9ab6d97
nisitasubudhi <162950721+nisitasubudhi@users.noreply.github.com>
2024-07-24 17:21:43 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,6 @@ sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/nisitasubudhi/fish.git
 sudo yum -y install python3-pip
-cd fish
+cd /fish
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
